58eb76aaf6480ad0b9e40e19af7b924eb3231b76
Nicolas Barray <nbarray@gmail.com>
2021-10-21 16:46:50 +0200

    test: add boilerplate for unit tests (#8)

---
+++ test/with-csv.ts
@@ -0,0 +1,28 @@
+import { withCSV } from "../src/index";
+import expect from "expect";
+import { execute, it } from "./test-suite";
+
+execute([
+  it('should load an array of the "First name" column', async () => {
+    expect(
+      await withCSV("test/data/small.csv").query(["First name"]).toArray()
+    ).toEqual([
+      { "First name": "Aloysius" },
+      { "First name": "University" },
+      { "First name": "Gramma" },
+      { "First name": "Electric" },
+      { "First name": "Fred" },
+      { "First name": "Betty" },
+      { "First name": "Cecil" },
+      { "First name": "Bif" },
+      { "First name": "Andrew" },
+      { "First name": "Jim" },
+      { "First name": "Art" },
+      { "First name": "Jim" },
+      { "First name": "Ima" },
+      { "First name": "Benny" },
+      { "First name": "Boy" },
+      { "First name": "Harvey" },
+    ]);
+  }),
+]);
