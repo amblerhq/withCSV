38552b8d4911ce376b592fff34df04a56a8441e0
Damien <dam.buty@gmail.com>
2022-11-12 00:38:30 +0100

    added test suites

---
+++ tests/core/chainable.test.ts
@@ -0,0 +1,84 @@
+import expect from 'expect'
+import {execute, it} from '..'
+import {withCSV} from '../../src'
+
+const data = `id,name,age
+1,Joe,23
+2,Mike,25
+3,Alfred,67
+4,Marcel,8
+`
+const dataWithDuplicates = `${data}
+1,Joe,23
+1,Balthazar,23
+23,Balthuzor,1
+24,Mike,25
+`
+
+execute('Chainable methods', [
+  it('map', async () => {
+    const buffer = Buffer.from(data)
+
+    const result = await withCSV(buffer)
+      .columns(['id', 'name', 'age'])
+      .map(row => `${row.name} - ${row.age}`)
+      .rows()
+
+    expect(result).toEqual(['Joe - 23', 'Mike - 25', 'Alfred - 67', 'Marcel - 8'])
+  }),
+
+  it('filter', async () => {
+    const buffer = Buffer.from(data)
+
+    const result = await withCSV(buffer)
+      .columns(['id', 'name', 'age'])
+      .filter(row => row.name.startsWith('M'))
+      .rows()
+
+    expect(result).toEqual([
+      {id: '2', name: 'Mike', age: '25'},
+      {id: '4', name: 'Marcel', age: '8'},
+    ])
+  }),
+
+  it('forEach', async () => {
+    const buffer = Buffer.from(data)
+    let count = 0
+
+    await withCSV(buffer)
+      .columns(['id'])
+      .forEach(() => {
+        count++
+      })
+      .process()
+
+    expect(count).toBe(4)
+  }),
+
+  it('uniq with one column name', async () => {
+    const buffer = Buffer.from(dataWithDuplicates)
+
+    const result = await withCSV(buffer).columns(['id', 'name', 'age']).uniq('id').rows()
+
+    expect(result.filter(row => row.id === '1').length).toBe(1)
+  }),
+
+  it('uniq with n column names', async () => {
+    const buffer = Buffer.from(dataWithDuplicates)
+
+    const result = await withCSV(buffer).columns(['id', 'name', 'age']).uniq(['age', 'name']).rows()
+
+    expect(result.filter(row => row.age === '25' && row.name === 'Mike').length).toBe(1)
+  }),
+
+  it('uniq with callback', async () => {
+    const buffer = Buffer.from(dataWithDuplicates)
+
+    const result = await withCSV(buffer)
+      .columns(['id', 'name', 'age'])
+      .uniq(row => `${parseInt(row.id) + parseInt(row.age)}`)
+      .rows()
+
+    expect(result.filter(row => parseInt(row.id) + parseInt(row.age) === 24).length).toBe(1)
+  }),
+])
---
+++ tests/core/terminator.test.ts
@@ -0,0 +1,166 @@
+import expect from 'expect'
+import {execute, it} from '..'
+import {withCSV} from '../../src'
+
+const data = `id,name,age
+1,Joe,23
+2,Mike,25
+3,Alfred,67
+4,Marcel,8
+`
+
+execute('Terminator methods', [
+  it('process', async () => {
+    const buffer = Buffer.from(data)
+    let count = 0
+
+    await withCSV(buffer)
+      .columns(['id'])
+      .forEach(() => {
+        count++
+      })
+      .process()
+
+    expect(count).toBe(4)
+  }),
+
+  it('find returns found row', async () => {
+    const buffer = Buffer.from(data)
+
+    const result = await withCSV(buffer)
+      .columns(['id', 'name', 'age'])
+      .find(row => row.name === 'Mike')
+
+    expect(result).toEqual({id: '2', name: 'Mike', age: '25'})
+  }),
+
+  it('find returns null', async () => {
+    const buffer = Buffer.from(data)
+
+    const result = await withCSV(buffer)
+      .columns(['id', 'name', 'age'])
+      .find(row => row.name === 'Carmichael')
+
+    expect(result).toEqual(null)
+  }),
+
+  it('findIndex returns found row index', async () => {
+    const buffer = Buffer.from(data)
+
+    const result = await withCSV(buffer)
+      .columns(['id', 'name', 'age'])
+      .findIndex(row => row.name === 'Mike')
+
+    expect(result).toBe(1)
+  }),
+
+  it('findIndex returns -1', async () => {
+    const buffer = Buffer.from(data)
+
+    const result = await withCSV(buffer)
+      .columns(['id', 'name', 'age'])
+      .findIndex(row => row.name === 'Carmichael')
+
+    expect(result).toBe(-1)
+  }),
+
+  it('every returns true', async () => {
+    const buffer = Buffer.from(data)
+
+    const result = await withCSV(buffer)
+      .columns(['id', 'name', 'age'])
+      .every(row => parseInt(row.age) < 150)
+
+    expect(result).toBe(true)
+  }),
+
+  it('every returns false', async () => {
+    const buffer = Buffer.from(data)
+
+    const result = await withCSV(buffer)
+      .columns(['id', 'name', 'age'])
+      .every(row => parseInt(row.age) < 50)
+
+    expect(result).toBe(false)
+  }),
+
+  it('some returns true', async () => {
+    const buffer = Buffer.from(data)
+
+    const result = await withCSV(buffer)
+      .columns(['id', 'name', 'age'])
+      .some(row => parseInt(row.age) > 50)
+
+    expect(result).toBe(true)
+  }),
+
+  it('some returns false', async () => {
+    const buffer = Buffer.from(data)
+
+    const result = await withCSV(buffer)
+      .columns(['id', 'name', 'age'])
+      .some(row => parseInt(row.age) > 150)
+
+    expect(result).toBe(false)
+  }),
+
+  it('includes returns false', async () => {
+    const buffer = Buffer.from(data)
+
+    const result = await withCSV(buffer).columns(['id', 'name', 'age']).includes({id: '3', name: 'Alfred', age: '67'})
+
+    expect(result).toBe(true)
+  }),
+
+  it('includes returns false', async () => {
+    const buffer = Buffer.from(data)
+
+    const result = await withCSV(buffer)
+      .columns(['id', 'name', 'age'])
+      .includes({id: '666', name: 'Petyr', age: '3500'})
+
+    expect(result).toBe(false)
+  }),
+
+  it('first', async () => {
+    const buffer = Buffer.from(data)
+
+    const result = await withCSV(buffer).columns(['id']).first(2)
+
+    expect(result.length).toBe(2)
+    expect(result).toEqual([{id: '1'}, {id: '2'}])
+  }),
+
+  it('last', async () => {
+    const buffer = Buffer.from(data)
+
+    const result = await withCSV(buffer).columns(['id']).last(2)
+
+    expect(result.length).toBe(2)
+    expect(result).toEqual([{id: '3'}, {id: '4'}])
+  }),
+
+  it('pick', async () => {
+    const buffer = Buffer.from(data)
+
+    const result = await withCSV(buffer).columns(['id', 'name']).pick('id')
+
+    expect(result).toEqual([{id: '1'}, {id: '2'}, {id: '3'}, {id: '4'}])
+  }),
+
+  it('key', async () => {
+    const buffer = Buffer.from(data)
+
+    const result = await withCSV(buffer).columns(['id', 'name']).key('id')
+
+    expect(result).toEqual(['1', '2', '3', '4'])
+  }),
+
+  it('toJSON', async () => {
+    const buffer = Buffer.from(data)
+
+    const result = await withCSV(buffer).columns(['id']).toJSON()
+
+    expect(result).toBe(`[{"id":"1"},{"id":"2"},{"id":"3"},{"id":"4"}]`)
+  }),
+])
