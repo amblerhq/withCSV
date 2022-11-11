005f0a2beaed45ab2704b5ca15f045292013b77c
Damien <dam.buty@gmail.com>
2022-11-11 18:33:40 +0100

    bump packages

--- test/test-suite.ts
+++ test/test-suite.ts
@@ -1,44 +1,42 @@
-import { BError } from "berror";
-import chalk from "chalk";
+import {BError} from 'berror'
+import chalk from 'chalk'
 
 export const it = (message: string, callback: () => Promise<void>) => {
   return {
     message,
     callback,
-  };
-};
+  }
+}
 
 export async function execute(testSuite: ReturnType<typeof it>[]) {
-  let i = 0;
-  let failed = 0;
-  let success = 0;
+  let i = 0
+  let failed = 0
+  let success = 0
   for await (let test of testSuite) {
-    i++;
+    i++
     try {
-      const { message, callback } = test;
+      const {message, callback} = test
       try {
-        await callback();
+        console.log(chalk.yellow(`=> ${message}`))
+        await callback()
       } catch (err: any) {
-        throw new BError(message, err);
+        throw new BError(message, err)
       }
-      success++;
+      success++
     } catch (err: any) {
       if (err instanceof BError) {
-        console.error(chalk.red("Test " + i + " failed:", err.message));
+        console.error(chalk.red('Test ' + i + ' failed:', err.message))
       } else {
-        console.error(
-          chalk.red("Unknown error in test " + i + ":", err.message)
-        );
-        throw err;
+        console.error(chalk.red('Unknown error in test ' + i + ':', err.message))
+        throw err
       }
-      failed++;
+      failed++
     }
   }
 
-  console.log();
-  console.log("Test suite ran with:");
-  console.log(chalk.blue(i + " tests"));
-  console.log(chalk.green(success + " success"));
-  console.log(chalk.red(failed + " failed"));
-  console.log();
+  console.log()
+  console.log(`Test suite ran with ${i} tests`)
+  console.log(chalk.green(success + ' success'))
+  console.log(chalk.red(failed + ' failed'))
+  console.log()
 }
